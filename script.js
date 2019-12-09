4431e4b56bbf46f22f875740f3812d56ef90dcf3
Cassidy Grom <cgrom@njadvancemedia.com>
2019-12-08 23:06:00 -0500

    made popup

--- script.js
+++ script.js
@@ -23,15 +23,35 @@ generateBtn.addEventListener("click", writePassword);
 
 // cassidy's pusedo code
 
+//FUNCTIONS
+
+//VARIABLES
+// ===============================================================================
+
 //Create a for loop that will run until all questions are answered
 
-//alert asking "pick a number between 8 and 128, this will be the length of your password" use variable n
+//prompt asking "pick a number between 8 and 128, this will be the length of your password" use variable n
+
+while (length !== null) {
+  var length = prompt(
+    "Pick a number between 8 and 128, this will be the length of your Password."
+  );
+
+  // if (length === "8") {
+  //   thanks();
+  // }
+}
+//if picks a number between 8 and 128, move on.
+
+//else prompt them to pick again. Say "Hey, you did something wrong. We said between 8 and 12"
 
 //if picks a number between 8 and 128, move on.
 
 //else prompt them to pick again. Say "Hey, you did something wrong. We said between 8 and 12"
 
 //Alert saying "Do you want the password to include a. special characters b. numeric characters c. lowercase characters or d. uppercase characters. Choose one: a, b, c or d."
+
+// Looping through each item in the array and logging a message to the console.
 
 //if picks one, move on
 
